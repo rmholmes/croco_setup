19e1b8d527483a511465a3fee5e926f6a87b7a22
Ryan Holmes <holmes.ryan.m@gmail.com>
2021-02-20 14:39:50 +1100

    Add general config files for gadi and structure setup stuff.

---
+++ setup_gadi_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Setup environment for CROCO on Gadi:
+module purge
+module load intel-compiler/2019.5.281
+module load netcdf/4.7.1
+module load openmpi/4.0.1
---
+++ setup_new_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Setup directory structure for a new clean configuration
+
+name='Benguela_LR'
+
+cd ~/croco
+
+# make config dir and copy source files:
+mkdir $name
+cp croco_src/OCEAN/cppdefs.h $name/
+cp croco_src/OCEAN/param.h $name/
+cp croco_tools_src/crocotools_param.m $name/
+cp croco_tools_src/start.m $name/
+cp croco_src/OCEAN/croco.in $name/
+
+# Copy my config setup files:
+cp submit_croco.sub $name/
+cp setup_gadi_env.sh $name/
+cp jobcomp $name/
+
+# setup links in directory:
+cd $name
+mkdir /scratch/e14/rmh561/croco/$name
+ln -s /scratch/e14/rmh561/croco/$name rundir
+mkdir rundir/OUT
+mkdir rundir/FRC
+mkdir rundir/Compile
+mkdir rundir/INI
