3a5b8318c3c87fcac17276b737f78b9b112042d0
Ryan Holmes <holmes.ryan.m@gmail.com>
2021-12-30 11:35:29 +1100

    Updates while getting oasis working.

--- setup_gadi_env.sh
+++ setup_gadi_env.sh
@@ -3,4 +3,7 @@
 module purge
 module load intel-compiler/2019.5.281
 module load netcdf/4.7.1
-module load openmpi/4.0.1
+module load openmpi/4.0.2
+# module use ~access/modules
+# module load oasis3-mct-local/ompi.4.0.2
+
